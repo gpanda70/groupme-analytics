5702c8781736e042f1ceac7f653da901a4cf942e
gpanda70 <gpanda@uga.edu>
2018-06-22 16:30:24 -0400

    Initial Boilerplate Design

---
+++ app.py
@@ -0,0 +1,161 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+
+import pandas as pd
+from datetime import datetime
+import dataprep as dp
+
+
+access_token = 'WgdkiHLjL5Qe0AgGUhqAnXExQuPQIdvah67xTDQr'
+group_id = '13388728'
+gen_date = str(datetime.now())
+
+df = dp.save_load_transform(access_token, group_id)
+final_df = pd.concat([df,pd.read_csv('src', sep='\t', encoding='utf-8')], ignore_index=True)
+final_df = final_df.loc[(final_df.real_names!='GroupMe') & (final_df.real_names!='GroupMe Calendar') & (final_df.real_names!='Paul Joon Kim'), :]
+
+proportions = dp.get_proportions(final_df)
+
+pvt =  final_df.pivot_table(values = 'favorited_count', index='real_names',aggfunc=['sum', 'count', 'mean'])
+pvt1 = final_df[(final_df['favorited_count']>0) | (final_df['real_names']=='Paul Joon Kim')].pivot_table(values = 'favorited_count', index='real_names',aggfunc=['sum', 'count', 'mean'])
+pvt['difference'] = pvt.loc[:,('count','favorited_count')] - pvt1.loc[:,('sum','favorited_count')]
+pvt.sort_values(by = ('count', 'favorited_count'),ascending=True,inplace=True)
+
+count_pvt = pvt.loc[:,('count', 'favorited_count')].sort_values(ascending=True)
+sum_pvt = pvt1.loc[:,('count', 'favorited_count')].sort_values(ascending=True)
+
+print(pvt)
+
+total_msg = len(final_df)
+total_mem = len(sum_pvt)
+
+
+app = dash.Dash()
+
+app.layout = html.Div(children = [
+    html.H1(children = 'GroupMe Analytics'),
+
+    html.Div(children =
+             '%d Members | %d Messages | Analytics generated as of %s' %(total_mem, total_msg, gen_date)),
+
+    dcc.Graph(
+        id='bar-graph',
+        figure={
+            'data': [
+                go.Bar(
+                    x=pvt['difference'],
+                    y=pvt.index,
+                    orientation='h',
+                    name='No Likes'
+                ),
+
+                go.Bar(
+                    x=sum_pvt,
+                    y=sum_pvt.index,
+                    orientation='h',
+                    name='At least 1 Like'
+                )
+            ],
+            'layout':
+                go.Layout(
+                    title='Messages Posted',
+
+                    barmode= 'stack',
+
+                    margin={
+                    'l': 110,  # left margin, in px
+                    'r': 15,  # right margin, in px
+                    't': 40,  # top margin, in px
+                    'b': 40  # bottom margin, in px
+                    },
+
+                    yaxis={'ticklen': 8, 'tickcolor': '#FFF'}
+                )
+
+
+
+        }
+    ),
+    html.Div([
+
+        dcc.Dropdown(
+            id='opt-dropdown',
+            options=[{'label': key, 'value': key} for key, i in proportions.items()],
+            value='Andrew Wardlaw'
+        ),
+
+        dcc.Graph(
+            id='donut-chart',
+            figure={
+                "data": [
+                    {
+                      "values": proportions['Andrew Wardlaw']*100,
+                      "labels": proportions['Andrew Wardlaw'].index,
+                      "domain": {"x": [0, .48]},
+                      "name": "This is the % of Likes attributed to this person",
+                      "hoverinfo":"label+percent+name",
+                      "hole": .4,
+                      "type": "pie"
+                    }
+                ],
+                "layout": {
+                    "title": "Who Likes You",
+                    "annotations": [
+                        {
+                            "font": {
+                                "size": 10
+                            },
+                            "showarrow": False,
+                            "text": "<b>Andrew<\b>",
+                            "x": 0.21,
+                            "y": 0.5
+                        }
+                    ]
+                }
+            }
+        ),
+
+
+    ],style={'width': '49%', 'display': 'inline-block', 'float': 'middle','margin-top': '10'}
+    )
+])
+@app.callback(
+    dash.dependencies.Output('donut-chart', 'figure'),
+    [dash.dependencies.Input('opt-dropdown', 'value')]
+)
+def update_pie_probablity(name):
+    return {
+                "data": [
+                    {
+                      "values": proportions[name]*100,
+                      "labels": proportions[name].index,
+                      "domain": {"x": [0, .48]},
+                      "name": "This is the % of Likes attributed to this person",
+                      "hoverinfo":"label+percent+name",
+                      "hole": .4,
+                      "type": "pie"
+                    }
+                ],
+                "layout": {
+                    "title": "Who Likes You",
+                    "annotations": [
+                        {
+                            "font": {
+                                "size": 10
+                            },
+                            "showarrow": False,
+                            "text": '<b>'+name.split(' ')[0]+'<\b>',
+                            "x": 0.21,
+                            "y": 0.5
+                        }
+                    ]
+                }
+            }
+
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=False, port=8000, host='0.0.0.0')
---
+++ dataprep.py
@@ -0,0 +1,107 @@
+import requests
+import pandas as pd
+import numpy as np
+
+user_map = {'10509565': 'Paul Joon Kim', '10956238': 'Cody Walls', '12719684': 'Christian Cremo',
+            '13130965': 'Stephen Osborn', '16034819': 'George Barron', '16923137': 'Scott Tippins',
+            '21051438': 'Andrew Wardlaw', '22705519': 'James Im', '22705520': 'Steven Hancock',
+            '24129141': 'Friedrich Neat', '6667755': 'Jack Harris', 'calendar': 'GroupMe Calendar',
+            'system': 'GroupMe', 10509565: 'Paul Joon Kim', 10956238: 'Cody Walls', 12719684: 'Christian Cremo',
+            13130965: 'Stephen Osborn', 16034819: 'George Barron', 16923137: 'Scott Tippins',
+            21051438: 'Andrew Wardlaw', 22705519: 'James Im', 22705520: 'Steven Hancock',
+            24129141: 'Friedrich Neat', 6667755: 'Jack Harris'}
+
+def data_pull(last_msg_id,access_token,group_id):
+
+    user = []
+    created_at = []
+    text = []
+    sender_id = []
+    user_id = []
+    favorited_by = []
+    message_id = []
+    Done = False
+
+    payload = {'after_id': last_msg_id}
+    response = requests.get('https://api.groupme.com/v3/groups/' + group_id + '/messages?token=' + access_token,
+                            params=payload)
+    data = response.json()
+
+    while Done==False:
+
+        try:
+
+            for i in range(0,20):
+                user.append(data['response']['messages'][i]['name'])
+                created_at.append(data['response']['messages'][i]['created_at'])
+                text.append(data['response']['messages'][i]['text'])
+                sender_id.append(data['response']['messages'][i]['sender_id'])
+                user_id.append(data['response']['messages'][i]['user_id'])
+                favorited_by.append(data['response']['messages'][i]['favorited_by'])
+                message_id.append(data['response']['messages'][i]['id'])
+
+
+            payload = {'after_id': message_id[-1]}
+            response = requests.get('https://api.groupme.com/v3/groups/' + group_id + '/messages?token=' + access_token,
+                                    params=payload)
+            data = response.json()
+
+        except IndexError:
+            Done = True
+
+    df = pd.DataFrame({'user': user,
+                       'create_date': created_at,
+                       'text': text,
+                       'sender_id': sender_id,
+                       'user_id': user_id,
+                       'favorited_by': favorited_by,
+                       'message_id': message_id})
+
+    return df[::-1]
+
+
+def transform_data(df):
+
+    df.to_csv('temp_file', sep='\t', encoding='utf-8', index=False)
+
+    t_df = pd.read_csv('temp_file', sep='\t', encoding='utf-8')
+
+    #transforms empty list to null value
+    t_df['favorited_by'] = t_df['favorited_by'].apply(lambda x: np.nan if x == '[]' else x)
+
+    #Finds the count of favorited messages
+    t_df['favorited_count'] = t_df['favorited_by'].apply(lambda x: 0 if pd.isnull(x) \
+                            else (len(x.split(',')) if ',' in x else 1))
+    #maps names
+    t_df['real_names'] = t_df['user_id'].map(user_map)
+
+
+    return(t_df)
+
+
+def save_load_transform(access_token,group_id):
+    last_msg = pd.read_csv('src', sep='\t', encoding='utf-8')['message_id'].iat[0]
+    new_df = data_pull(last_msg,access_token,group_id)
+    t_df = transform_data(new_df)
+    return t_df
+
+def get_proportions(final_df):
+    proportions = {}
+    pvt = final_df.pivot_table(values = 'favorited_count', index='real_names',aggfunc=['sum', 'count', 'mean'])
+    for person in pvt.index:
+        p = final_df.loc[(final_df.real_names == person) & (final_df.favorited_by.isnull() == False), :].favorited_by.apply(
+            lambda x: x.replace('[', ''))
+        p = p.apply(lambda x: x.replace(']', ''))
+        p = pd.DataFrame(p.str.split(', ').tolist()).stack()
+        p = p.reset_index(drop=True)
+        p = p.apply(lambda x: x.replace('\'', ''))
+        p = p.map(user_map)
+        proportions[person] = p.value_counts(normalize=True)
+
+    return proportions
+#access_token = 'WgdkiHLjL5Qe0AgGUhqAnXExQuPQIdvah67xTDQr'
+#group_id = '13388728'
+
+#ast_msg_id = pd.read_csv('src', sep='\t', encoding='utf-8')['message_id'].iat[0]
+
+#print(data_pull(ast_msg_id, access_token, group_id))
