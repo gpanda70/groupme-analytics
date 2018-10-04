0308f4fd516af7caaaba69c22fee1747be247399
gpanda70 <gpanda@uga.edu>
2018-10-04 14:19:17 -0400

    added clock.py to run update jobs

--- clock.py
+++ clock.py
@@ -11,6 +11,8 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', day_of_week='mon', hour=1, timezone='America/New_York')
 def scheduled_job():
+    file_path = os.path.join(os.path.dirname(__file__),'src')
+    msg = Message(access_token, group_id,file_path)
     msg.load()
     msg.update()
     msg.save()
